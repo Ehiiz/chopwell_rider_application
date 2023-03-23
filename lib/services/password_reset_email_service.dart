3cff16af4244dd4d2b0bc3b2db50b6473c30b40f
Ehiiz <62038343+Ehiiz@users.noreply.github.com>
2023-03-23 09:56:35 +0100

    Fixed all services

--- lib/services/password_reset_email_service.dart
+++ lib/services/password_reset_email_service.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
-import 'package:chopwell_rider_application/hooks/request_module.dart';
+import 'package:chopwell_rider_application/models/response_models/error_response_model.dart';
+import 'package:chopwell_rider_application/utils/request_module.dart';
 import 'package:chopwell_rider_application/models/request_models/verify_email_request_model.dart';
 import 'package:chopwell_rider_application/models/response_models/string_based_response_model.dart';
+
+import 'complete_account_service.dart';
 
 class PasswordResetEmailService {
   static const String _passwordResetPath = '/rider/password-reset/email';
@@ -14,10 +17,19 @@ class PasswordResetEmailService {
 
     if (response.statusCode == 200) {
       final responseMap = json.decode(response.body);
-      final decodedResponse = StringDataResponseModel.fromJson(responseMap);
-      return decodedResponse;
+      if (responseMap["data"] != null) {
+        final decodedResponse = StringDataResponseModel.fromJson(responseMap);
+        return decodedResponse;
+      }
+
+      final decodedResponse = ErrorResponseModel.fromJson(responseMap);
+      return convertStringErrorResponse(decodedResponse);
     } else {
-      throw Exception('Unable verify email');
+      final responseMap = json.decode(response.body);
+
+      final decodedResponse = ErrorResponseModel.fromJson(responseMap);
+      return convertStringErrorResponse(decodedResponse);
+      // throw Exception("Unable to set up working hours");
     }
   }
 }
