89d2dc73a9e7a73d8624c997079d3ef9d080052c
Ehiiz <ehisboy91@gmail.com>
2023-10-03 12:56:00 -0400

    Fixed podfiles

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by MAC on 10/3/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
